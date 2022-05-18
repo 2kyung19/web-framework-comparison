c8343f27424c0a0e094d19760b4f0cd5cace6176
billie.dev(이경은)/dkt <2kyung19@gmail.com>
2022-05-18 09:20:19 +0900

    chore : 절대경로 설정

--- react-proj/src/index.tsx
+++ react-proj/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from 'reportWebVitals';
 
-import Home from './pages/Home';
+import Home from 'pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
